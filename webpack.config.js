6a17e87040360dac05a67824da5e30b751c68ca3
Jan Biasi <jan.biasi@namics.com>
2017-07-26 17:03:28 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.ts",
+    output: {
+        filename: "./dist/bundle.js"
+    },
+    resolve: {
+        extensions: ["", ".ts", ".js"]
+    },
+    module: {
+        loaders: [{ test: /\.ts$/, loader: "ts-loader" }]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+            host: "localhost",
+            port: 3000,
+            server: { baseDir: ["dist"] }
+        })
+    ]
+};
